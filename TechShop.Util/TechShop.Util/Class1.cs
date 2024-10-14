92d37cd658df18519a47845b0d8a6f30a1a7ab55
Srinithi R <129388242+Srinithi2211@users.noreply.github.com>
2024-10-14 17:27:25 +0530

    Add files via upload

---
+++ TechShop.Util/TechShop.Util/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data.SqlClient;
+using TechShop.businesslayer;
+using TechShop.entity;
+
+
+namespace TechShop.Util
+{
+    public static class DBConnUtil
+    {
+        public static SqlConnection GetConnection(string connectionString)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Connection established.");
+                return connection;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                throw;  // Re-throw the exception to handle it elsewhere if needed
+            }
+        }
+
+        public static void CloseConnection(SqlConnection connection)
+        {
+            if (connection != null && connection.State == System.Data.ConnectionState.Open)
+            {
+                connection.Close();
+                Console.WriteLine("Connection closed.");
+            }
+        }
+    }
+
+}
